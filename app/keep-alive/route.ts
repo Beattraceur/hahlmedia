05dee03c82fb69f711ee8b337ea38ad1daa6bf19
Benjamin Hahl <contact@hahl.media>
2024-07-10 10:56:22 +0200

    cron job for admin panel

--- app/keep-alive/route.ts
+++ app/keep-alive/route.ts
@@ -3,6 +3,6 @@ import { NextResponse } from 'next/server';
 export const revalidate = 0;
 
 export async function GET() {
-  await fetch(`https://${process.env.VERCEL_URL}/`);
+  await fetch(`https://${process.env.VERCEL_URL}/admin`);
   return NextResponse.json({ ok: true });
 }
