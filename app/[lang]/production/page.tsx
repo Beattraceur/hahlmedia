f74ef59e21f862b3d092ccfa792306f5f3402135
Benjamin Hahl <contact@hahl.media>
2024-07-11 11:59:44 +0200

    fixing heroPic for every Page

--- app/[lang]/production/page.tsx
+++ app/[lang]/production/page.tsx
@@ -1,40 +1,40 @@
-import ThemedImage from "@/components/custom-ui/ThemedImage";
-import { Locale } from "@/i18n.config";
-import { PagePictureLoader, PageTextLoader } from "@/lib/payloadLoader";
-import { PagePicture, PageText } from "@/lib/types";
-import { Metadata } from "next";
+import ThemedImage from '@/components/custom-ui/ThemedImage';
+import { Locale } from '@/i18n.config';
+import { PagePictureLoader, PageTextLoader } from '@/lib/payloadLoader';
+import { PagePicture, PageText } from '@/lib/types';
+import { Metadata } from 'next';
 //Comments found in "home"-page
 export const metadata: Metadata = {
-  title: "hahl - Media Production",
-  description: "Mediaproduction Portfolio by Benjamin Hahl",
+  title: 'hahl - Media Production',
+  description: 'Mediaproduction Portfolio by Benjamin Hahl',
 };
 export default async function Production({
   params: { lang },
 }: {
   params: { lang: Locale };
 }) {
-  const heroPic = (await PagePictureLoader({
-    slug: "production",
-    param: "hero",
+  const Pic = (await PagePictureLoader({
+    slug: 'production',
+    param: 'hero',
   })) as PagePicture;
-  const heroPic_dark = (await PagePictureLoader({
-    slug: "production_dark",
-    param: "hero",
+  const Pic_dark = (await PagePictureLoader({
+    slug: 'production_dark',
+    param: 'hero',
   })) as PagePicture;
-  const text = (await PageTextLoader({ slug: "production" })) as PageText;
+  const text = (await PageTextLoader({ slug: 'production' })) as PageText;
 
   return (
     <>
-      {heroPic && (
+      {Pic && (
         <ThemedImage
-          heroPic={heroPic}
-          heroPic_dark={heroPic_dark}
-          classes="w-full max-h-64 object-cover"
+          Pic={Pic}
+          Pic_dark={Pic_dark}
+          classes='w-full max-h-64 object-cover'
         />
       )}
       {text && (
-        <div className="p-24 ">
-          <h1 className="text-3xl font-bold ">{text[`${lang}_title`]}</h1>
+        <div className='p-24 '>
+          <h1 className='text-3xl font-bold '>{text[`${lang}_title`]}</h1>
           <p>{text[`${lang}_description`]}</p>
         </div>
       )}
